3ae50ccec18240d3e4b427599c099c5383617d2b
Manh-Ha VU <dev@manhhavu.com>
2022-02-21 06:45:18 +0100

    Add SectionDataProcessor

--- cvraft/extension.py
+++ cvraft/extension.py
@@ -3,12 +3,13 @@ import xml.etree.ElementTree as etree
 
 from markdown import Markdown
 from markdown.blockprocessors import BlockProcessor
+from markdown.treeprocessors import Treeprocessor
 from markdown.extensions import Extension
 
 
 class WrapSectionsProcessor(BlockProcessor):
-    STATE_NAME = 'wrapping-%s'
-    RE = re.compile(r'(?:^|\n)(?P<level>#{1,7})(?P<header>(?:\\.|[^\\])*?)#*(?:\n|$)')
+    STATE_NAME = "wrapping-%s"
+    RE = re.compile(r"(?:^|\n)(?P<level>#{1,7})(?P<header>(?:\\.|[^\\])*?)#*(?:\n|$)")
 
     def __init__(self, parser):
         super().__init__(parser)
@@ -16,27 +17,27 @@ class WrapSectionsProcessor(BlockProcessor):
     def test(self, parent: etree.ElementTree, block: str) -> None:
         m = self.RE.search(block)
         if m:
-            level = m.group('level')
-            return not self.parser.state.isstate(self.STATE_NAME % len(level)) 
+            level = m.group("level")
+            return not self.parser.state.isstate(self.STATE_NAME % len(level))
         return False
 
     def run(self, parent: etree.ElementTree, blocks: list[str]) -> None:
         block = blocks.pop(0)
         m = self.RE.search(block)
 
-        if m:                    
-            level = m.group('level')
+        if m:
+            level = m.group("level")
             self.parser.state.set(self.STATE_NAME % len(level))
-            blocks_in_section = [block[m.start():m.end()]]
-            blocks.insert(0, block[m.end():])
+            blocks_in_section = [block[m.start() : m.end()]]
+            blocks.insert(0, block[m.end() :])
 
-            NEXT_RE = re.compile(f'(?:^|\n)({level})[^#]+')
+            NEXT_RE = re.compile(f"(?:^|\n)({level})[^#]+")
 
             cur = -1
             for i, b in enumerate(blocks):
-                m2 = NEXT_RE.search(b) 
+                m2 = NEXT_RE.search(b)
                 if m2:
-                    blocks_in_section.append(b[:m2.start()])
+                    blocks_in_section.append(b[: m2.start()])
                     break
                 else:
                     cur = i
@@ -46,7 +47,7 @@ class WrapSectionsProcessor(BlockProcessor):
                     b = blocks.pop(0)
                     blocks_in_section.append(b)
 
-            section = etree.SubElement(parent, 'section')
+            section = etree.SubElement(parent, "section")
             self.parser.parseBlocks(section, blocks_in_section)
             self.parser.state.reset()
             return True
@@ -55,6 +56,42 @@ class WrapSectionsProcessor(BlockProcessor):
             return False
 
 
+HEADER_TAGS = ["h1", "h2", "h3", "h4", "h5", "h6", "h7"]
+
+
+def is_header(tag: str):
+    return tag.lower() in HEADER_TAGS
+
+
+class SectionDataProcessor(Treeprocessor):
+    SECTION_ID = "data-section-id"
+    SECTION_CLASS = "data-section-class"
+
+    def run(self, root):
+        for section in root.iter("section"):
+            self._move_section_data_from_heading(section)
+
+    def _move_section_data_from_heading(self, section):
+        for child in section:
+            if is_header(child.tag):
+                if self.SECTION_ID in child.attrib:
+                    section.attrib["id"] = child.attrib[self.SECTION_ID]
+                    child.attrib.pop(self.SECTION_ID)
+                if self.SECTION_CLASS in child.attrib:
+                    section.attrib["class"] = child.attrib[self.SECTION_CLASS]
+                    child.attrib.pop(self.SECTION_CLASS)
+            elif child.tag == "section":
+                self._move_section_data_from_heading(child)
+
+
 class CvraftExtension(Extension):
     def extendMarkdown(self, md: Markdown) -> None:
-        md.parser.blockprocessors.register(WrapSectionsProcessor(md.parser), "wrap-sections", 75)
+        md.parser.blockprocessors.register(
+            WrapSectionsProcessor(md.parser), "wrap-sections", 75
+        )
+        md.treeprocessors.register(SectionDataProcessor(md), "section-data", 7)
+
+
+# Recommend by Python Markdown: https://python-markdown.github.io/extensions/api/#dot_notation
+def makeExtension(**kwargs):
+    return CvraftExtension(**kwargs)
